af66e10ca871c0f5bd6492cf4ff3f9e6882cbc9d
luankz <17682333171@163.com>
2024-11-05 20:01:29 +0800

    chore: 简单封装 jumpserver

--- third_party/base/wechat.py
+++ third_party/base/wechat.py
@@ -7,3 +7,6 @@ class Wechat:
 
     def get_user_info(self, user_id: str):
         return self.client.user.get(user_id)
+
+    def add_media(self, media_type, media_file):
+        return self.client.media.upload(media_type, media_file)
